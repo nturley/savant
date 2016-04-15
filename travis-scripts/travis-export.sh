1440f761d06000680b4a11bbde0fe1ba2672a5e3
Neil Turley <neilpturley@gmail.com>
2016-04-15 13:24:47 -0500

    add scram binary executable to tarball

--- travis-scripts/travis-export.sh
+++ travis-scripts/travis-export.sh
@@ -6,10 +6,12 @@ else
 sudo make install
 mkdir export/include -p
 mkdir export/lib/savant -p
+mkdir export/bin -p
 cp /usr/local/include/savant export/include -rv
 cp /usr/local/lib/*savant* export/lib -v
 cp /usr/local/lib/*aire* export/lib -v
 cp /usr/local/lib/savant export/lib -rv
+cp /usr/local/bin/scram export/bin -v
 mkdir deploy -p
 tar -zcvf deploy/savant.tar.gz export
 fi
