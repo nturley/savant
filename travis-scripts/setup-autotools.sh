6102dfe5c4a4c152c46b827d4466dcc71643f7f6
Neil Turley <neilpturley@gmail.com>
2016-04-15 15:00:55 -0500

    add plugin autotools setup

--- travis-scripts/setup-autotools.sh
+++ travis-scripts/setup-autotools.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+if test "$BUILDING" = "BINARY"; then
 aclocal
 autoconf
 libtoolize
 automake --add-missing
+elif test $BUILDING = "PLUGIN"; then
+cd doc/programmers
+aclocal
+autoconf
+libtoolize
+automake --add-missing
+cd ../..
+fi
+
