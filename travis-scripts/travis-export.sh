7a9226a8c1025a373eb26a8346de54b70f75a6eb
Neil Turley <neilpturley@gmail.com>
2016-04-15 10:39:46 -0500

    add binary export step

--- travis-scripts/travis-export.sh
+++ travis-scripts/travis-export.sh
@@ -4,5 +4,12 @@ mkdir deploy/savant-dox -p
 cp doc/html/* deploy/savant-dox
 else
 sudo make install
+mkdir export/include -p
+mkdir export/lib/savant -p
+cp /usr/local/include/savant export/include -rv
+cp /usr/local/lib/*savant* export/lib -v
+cp /usr/local/lib/*aire* export/lib -v
+cp /usr/local/lib/savant export/lib -rv
 mkdir deploy -p
+tar -zcvf deploy/savant.tar.gz export
 fi
